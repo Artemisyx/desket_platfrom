6940053a712938a9742caf1081ebe20305de7b24
Artemisyx <yuanxiaoikk@163.com>
2023-04-19 16:17:01 +0800

    v1
    没有提交直接切换了分支，差点没了，虽然可能没有用，还是存一下，
    规范之类的，之后再弄

--- src/main/java/tyut/selab/desktop/moudle/tools/client/service/impl/FileService.java
+++ src/main/java/tyut/selab/desktop/moudle/tools/client/service/impl/FileService.java
@@ -7,83 +7,155 @@ import tyut.selab.desktop.moudle.tools.client.service.IFileUpService;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class FileService implements IFileUpService {
+
+    private IUserDao userDao;  //是学生类的一个接口  包含11个方法
+
+    private IUpFileDao upFileDao;  // 周报管理系统的Dao
+
+    String localPath;//          = "C:\\Users\\86184\\Desktop\\袁晓晶-2022005929.zip";     //Defaulted
+
     public static void main(String[] args) {
-
+        FileService fileService = new FileService();
+        fileService.userDao.queryUserByAccount()
     }
 
-    private IUserDao userDao;
-
-    private IUpFileDao upFileDao;
-    int port;
+    private final int port = 45617;
+    //构造器方法
+    public FileService(String localPath){
+        this.localPath = localPath;
+    }
 
     @Override
-    /*
-
-
-    需要的参数：
-    从UI/Dao层获取 下载文件信息
-    （向服务器发送需要下载的文件）
-    （用接受服务器的文件流）
-    从 dao层获取下载路径
-
-    * */
+    //下载方法
     public int fileDown(FileUpVo fileUpVo) {
+        String name ="Defaulted";     //第一部分为：姓名 （本以为从Student 的Dao层 获取）()
+        String week ="第一周";         //第二部分为周数，判断周数需要自己写（当然，不是这一部分的内容）
+        //Socket连接
+        Socket Socket;
+        //从本地输出到Socket
+        OutputStream outStream;       //Socket 创建输出流
+        DataOutputStream dataOutStream;
+        byte[] sendByte;              //输出数据
+        //从Socket输入
+        InputStream inputStream;      //Socket 创建输入流
+        DataInputStream dataInputStream;
+        //输入流输出到文件
+        FileOutputStream fileOutputStream;
 
+        try {
+            sendByte = new byte[39];            //7+6
+            fileByteArray(sendByte, name.getBytes(StandardCharsets.UTF_8), week.getBytes(StandardCharsets.UTF_8), 21);//// 调用方法 实现固定模式的byte数组 让剩下的部分被0填充
+            //建立套接字
+            Socket = new Socket("localhost",port);
 
+            //输出数据部分
+            outStream = Socket.getOutputStream();
+            dataOutStream = new DataOutputStream(outStream);//数据输出流
+            dataOutStream.write(sendByte);
+
+            System.out.println("连接到服务器成功"); // 终端
+
+            //输入部分
+            inputStream =Socket.getInputStream();
+            dataInputStream = new DataInputStream(inputStream);
+            fileOutputStream= new FileOutputStream(localPath);
+            byte[] data = new byte[5*1024];
+            while (true){
+                int len = dataInputStream.read(data);
+                if (len == -1){
+                    break;
+                }
+                System.out.println("len："+len);
+                fileOutputStream.write(data,0,len);
+            }
+            fileOutputStream.close();
+            dataInputStream.close();
+            dataOutStream.close();
+            Socket.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return 0;
     }
 
     @Override
-    //文件上传方法
-    //1. 需要判断字符串是否为upIp吗
-    //2. 端口号是固定的吗？
+    public int fileUpLoading(FileUpVo fileUpVo)  {
+        int length;
+        //套接字
+        Socket socket;
+        //输出流
+        OutputStream outputStream;
+        DataOutputStream dataOutputStream;
+      //  PrintWriter printWriter;
+        //从文件输入
+        FileInputStream fileInputStream;
+        //输出流的第一部分内容         ---->服务端路径 (从UI 或 数据库获取 ->调用fileUpTo???)
 
-    public int fileUpLoading(FileUpVo fileUpVo) {
-        String upIp = fileUpVo.getUpIp();
-        String upFilepath = fileUpVo.getUpFilePath();
-        int length = 0;
-        FileInputStream fis = null;
-        DataOutputStream dos = null;
-        Socket socket = null;
-        OutputStream out =null;
-        PrintWriter pw = null;
-        byte[] sendByte = null;
+        String path = fileUpVo.getUpFilePath();        //服务端路径 //查询之后获取服务端路径
+        int pathNum = 80;
+        String serverPath ="Defaulted";
+        byte[] serverPathByte;
+        //输出流的第二部分------->文件内容
+        String localPath = "Defaulted";               // 本地路径，也是需要获取的
+        byte[] fileByte ;               // 文件路径
+        byte[] sendBytes ;              // 上传数组
+
         try {
-            socket = new Socket(upIp,7777);
-            out = socket.getOutputStream();
-            pw = new PrintWriter(out);
-            System.out.println("连接到服务器成功");
-            File file = new File("C:\\Users\\86184\\Documents\\GitHub\\S__StudyNotes\\Back_ends_Learning\\L_Code\\A1_HTML\\A_img\\与松鼠的约定.png");
-            fis = new FileInputStream(file);
-            dos = new DataOutputStream(socket.getOutputStream());
-            sendByte = new byte[1024];
-            pw.write("123"+"\r\n");
-            pw.flush();
-            pw.write("456"+"\r\n");
-            pw.flush();
-            System.out.println("准备发送");
-            while((length=fis.read(sendByte))>0){
-                dos.write(sendByte, 0 , length);
-                dos.flush();
-            }
-            System.out.println("文件发送完毕");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }finally{
-            try {
-                fis.close();
-                dos.close();
-                socket.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        /*
-        上传是否成功 需要从服务器获得判断信息
-        */
-        return 0;
+            socket = new Socket("localhost",port);
+            outputStream = socket.getOutputStream();
+       //     printWriter = new PrintWriter(outputStream);
 
+
+            System.out.println("连接到服务器成功");
+            File file = new File(localPath);
+            fileInputStream = new FileInputStream(file);
+            dataOutputStream = new DataOutputStream(outputStream);
+            fileByte = new byte[1024*5];            //文件
+            serverPathByte = serverPath.getBytes(StandardCharsets.UTF_8);
+            sendBytes = new byte[1024*5+80];    //组合起来的第三个数组  有个流是无限流
+            fileByteArray(sendBytes,serverPathByte,fileByte,80);
+
+            while ((length=fileInputStream.read(sendBytes))>0){
+                dataOutputStream.write(sendBytes,0,length);
+            }
+        }catch (Exception e){
+          e.printStackTrace();
+        }
+
+        return 0;
     }
+
+    /**
+     *
+     * @param endByte  待填充数组
+     * @param data1    填充数组一
+     * @param data2    填充数组二
+     * @param length1  第一部分的长度
+     */
+
+    private static byte[] fileByteArray(byte[] endByte,byte[] data1, byte[] data2, int length1) {
+
+        fileByteArrayAct(endByte, data1, data2, length1);
+        // System.out.println(Arrays.toString(endByte));
+        return endByte;
+    }
+
+    public static void fileByteArrayAct(byte[] endByte, byte[] data1, byte[] data2, int length1) {
+        byte a = 0;
+        System.arraycopy(data1, 0, endByte, 0, data1.length);
+        Arrays.fill(endByte,data1.length,length1-1,a);
+        System.arraycopy(data2,0,endByte,length1,data2.length);
+        Arrays.fill(endByte,length1+data2.length,endByte.length,a);
+    }
+
 }
+
+//            printWriter.write("123"+"\r\n");
+//            printWriter.flush();
+//            printWriter.write("456+"+"\r\n");
+//            printWriter.flush();
+//            System.out.println("准备发送");
